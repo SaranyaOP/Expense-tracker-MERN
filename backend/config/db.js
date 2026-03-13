22c1312175ac99dd9055581481051f498ff76dee
SaranyaOP <saranyaop98@gmail.com>
2026-03-13 22:46:51 +0530

    Removed db link

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,2 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async () => {  
-    await mongoose.connect('mongodb+srv://saranyaop98_db_user:bcGWyaNzsgw1qAhH@cluster0.tuggnvy.mongodb.net/Expense')
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    
-}
